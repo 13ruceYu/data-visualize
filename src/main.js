1356d386feb28804846845d06cd6cfc42d8aff4c
Bruce Yu <bruceyuhongbo@gmail.com>
2021-02-09 17:26:20 +0800

    init again

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from 'axios'
 
-Vue.config.productionTip = false;
+// axios 基准路径配置
+axios.defaults.url = 'http://127.0.0.1:8888/api/'
+// axios 全局挂载
+Vue.prototype.$http = axios
+
+// 全局 echart 挂载
+Vue.prototype.$echarts = window.echarts
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
