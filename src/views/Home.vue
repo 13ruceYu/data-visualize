61ef8abea207d9739535710e33e707a2ba098488
13ruce Yu <bruceyuhongbo@gmail.com>
2021-02-23 15:24:42 +0800

    chore: test user

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1>home</h1>
+    <h1>hom</h1>
     <router-link to="/screenpage">screen-page</router-link><br />
     <router-link to="/electronicpage">electronic-page</router-link><br />
     <router-link to="/constructionpage">construction-page</router-link>
